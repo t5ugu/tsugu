c6efee4e67dc2dd981accec7d0015e2856418e3f
t5ugu <tsug0530@gmail.com>
2020-08-29 21:09:27 +0900

    0.1.0

--- src/extension.ts
+++ src/extension.ts
@@ -3,53 +3,38 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('"tsugu" is now active!');
 
-	let disposable = vscode.commands.registerCommand('tsugu.swap', () => {
+	let swap = vscode.commands.registerCommand('tsugu.swap', () => {
 		let editor = vscode.window.activeTextEditor;
 		let doc = editor?.document;
 		let cursors = editor?.selections;
 
-		let selectA: vscode.Selection;
-		let selectB: vscode.Selection;
+		let selectA: vscode.Selection,
+			selectB: vscode.Selection;
+
+		function selectString(c: vscode.Selection) {
+			let toVal = new vscode.Selection(c.start, c.end);
+			if (c.isEmpty) {
+				let range = doc?.getWordRangeAtPosition(c.start);
+
+				if (range?.isEmpty !== undefined) {
+					toVal = new vscode.Selection(range?.start, range?.end);
+				}
+			}
+			return toVal;
+		}
 
 		if (cursors?.length === 2) {
-
-			if (cursors[0].isEmpty) {
-				let rangeA = doc?.getWordRangeAtPosition(cursors[0].start);
-
-				if (rangeA?.isEmpty !== undefined) {
-					selectA = new vscode.Selection(rangeA?.start, rangeA?.end);
-				}
-				else {
-					selectA = new vscode.Selection(cursors[0].start, cursors[0].start);
-				}
-			}
-			else {
-				selectA = new vscode.Selection(cursors[0].start, cursors[0].end);
-			}
-
-			if (cursors[1].isEmpty) {
-				let rangeB = doc?.getWordRangeAtPosition(cursors[1].start);
-
-				if (rangeB?.isEmpty !== undefined) {
-					selectB = new vscode.Selection(rangeB?.start, rangeB?.end);
-				}
-				else {
-					selectB = new vscode.Selection(cursors[1].start, cursors[1].start);
-				}
-			}
-			else {
-				selectB = new vscode.Selection(cursors[1].start, cursors[1].end);
-			}
+			selectA = selectString(cursors[0]);
+			selectB = selectString(cursors[1]);
 
 			editor?.edit(edit => {
 				edit.replace(selectA, doc?.getText(selectB) + '');
 				edit.replace(selectB, doc?.getText(selectA) + '');
 			});
 		}
-		vscode.window.showInformationMessage('Swap!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(swap);
 }
 
 export function deactivate() { }
