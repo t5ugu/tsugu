c38eafb9c24e18abdd8011d260afaa18c51af57b
t5ugu <tsug0530@gmail.com>
2020-09-03 17:49:16 +0900

    0.2.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,19 @@
 import * as vscode from 'vscode';
 import { swap } from './swap';
 import { scoreOperation } from './score-operation';
+import { fullToHalf } from './full2half';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('"tsugu" is now active!');
-
-	let editor = vscode.window.activeTextEditor;
-	let doc = editor?.document;
 
 	let swapping = vscode.commands.registerCommand('tsugu.swap', () => swap());
 	context.subscriptions.push(swapping);
 
 	let operation = vscode.commands.registerCommand('tsugu.operation', () => scoreOperation());
 	context.subscriptions.push(operation);
+
+	let full2half = vscode.commands.registerCommand('tsugu.full2half', () => fullToHalf());
+	context.subscriptions.push(full2half);
 }
 
 export function deactivate() { }
---
+++ src/full2half.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+
+export function fullToHalf() {
+    let editor = vscode.window.activeTextEditor;
+    let doc = editor?.document;
+
+    let select = new vscode.Selection(new vscode.Position(0, 0),
+        new vscode.Position(doc!.lineCount - 1, 10000));
+    let text = doc!.getText(select);
+
+    for (let i = 0; i < text.length; i++) {
+        if (text.charAt(i) === '　') {
+            text = text.substring(0, i) + '  ' + text.substring(i + 1, text.length);
+        }
+    }
+
+    editor?.edit(edit => {
+        edit.replace(select, text);
+    });
+}
