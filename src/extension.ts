44307e5ea277c32e76c685f750a15e1e7d0f2dc2
t5ugu <tsugu0530@outlook.jp>
2020-11-19 19:57:25 +0900

    clear

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,7 @@
 import * as vscode from 'vscode';
-import { swap } from './swap';
-import { scoreOperation } from './score_ope/score_operation';
-import { fullToHalf } from './full2half';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('tsugu.swap',		() => swap()),
-		vscode.commands.registerCommand('tsugu.operation',	() => scoreOperation()),
-		vscode.commands.registerCommand('tsugu.full2half',	() => fullToHalf()),
-	);
+	context.subscriptions.push();
 }
 
 export function deactivate() { }
